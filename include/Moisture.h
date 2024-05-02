881402fdb056dbaa3bbe0e85f48f3ac23daab404
YanSchw <schoessow.yannick@gmail.com>
2024-05-02 14:47:37 +0200

    Added Soil Moisture Sensor

---
+++ include/Moisture.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+
+class Moisture
+{
+    static uint32_t GetMoisture();
+    static bool IsWet();
+};
---
+++ src/Moisture.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "Moisture.h"
+
+#define AOUT_PIN 36 // ESP32 pin GPIO36 (ADC0) that connects to AOUT pin of moisture sensor
+#define THRESHOLD 2800 // CHANGE YOUR THRESHOLD HERE
+
+uint32_t Moisture::GetMoisture()
+{
+    return analogRead(AOUT_PIN);
+}
+
+bool Moisture::IsWet()
+{
+    return GetMoisture() <= THRESHOLD;
+}
